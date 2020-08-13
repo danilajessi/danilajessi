f9b4de6ee8de3df68e6ceda5f7c238a5bd87cda4
danilajessi <68910042+danilajessi@users.noreply.github.com>
2020-08-13 09:18:39 -0300

    actualizacion para peimera entrega
    actuelizacion 13/8
    creacion de login y agregado de listado de productos

--- js/login.js
+++ js/login.js
@@ -3,4 +3,16 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+  const signUpButton = document.getElementById('signUp');
+  const signInButton = document.getElementById('signIn');
+  const container = document.getElementById('container');
+
+  signUpButton.addEventListener('click', () => {
+  	container.classList.add("right-panel-active");
+  });
+
+  signInButton.addEventListener('click', () => {
+  	container.classList.remove("right-panel-active");
+  });
+
 });
