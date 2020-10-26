7ff54bb365684fb3c216a9a98d8d908c0bf44a29
danilajessi <68910042+danilajessi@users.noreply.github.com>
2020-10-26 19:04:45 -0300

    entrega 6
    agrega elporsentaje de envio al valor final apagar, habre ventana modal con datos de terjeta, y valida campos

--- js/cart.js
+++ js/cart.js
@@ -12,7 +12,7 @@ function showCartProductsAndTotalCost(articles) {
             <td>`+ articles[i].name + `</td>
             <td>`+ articles[i].currency + " " + articles[i].unitCost + `</td>
             <td><input class="form-control article" style="width:60px;" type="number" id="productCount-${i}" value="` + articles[i].count + `" min="1"></td>
-            <td><span id="productSubtotal-${i}" style="font-weight:bold;">${articles[i].currency} ${cuenta(articles)}</span></td>
+            <td><span id="productSubtotal-${i}" style="font-weight:bold;">${articles[i].currency} ${articles[i].unitCost * articles[i].count}</span></td>
         </tr>
         `
     }
@@ -20,11 +20,7 @@ function showCartProductsAndTotalCost(articles) {
     addEventCount();
     updateAllSubTotal();
     totalFinal();
-}
-
-//primer calculo a mostrar
-function cuenta(array) {    
-    array.unitCost * array.count;
+    //envio(mostrarSubtotal);
 }
 
 // funcion que calcula el precio del producto y cambia la moneda para sumar los subtotales
@@ -43,11 +39,32 @@ function updateAllSubTotal() {
     let subtotalArray = document.getElementsByClassName("article");
     let subtotal = 0;
     for (let i = 0; i < subtotalArray.length; i++) {
-        subtotal += productSubTotal(subtotalArray[i].value, i);
+        subtotal += productSubTotal(subtotalArray[i].value, i);       
     }
     document.getElementById("subT").innerHTML = "USD " + subtotal;
     mostrarSubtotal = subtotal;
 }
+
+//funciones que actualizan el valor a pagar dependiendo del envio, reaccionan al onclick que tienen los respectivos botones
+function envio1() {
+    let subtotal = [];
+        subtotal = mostrarSubtotal * 1.15;
+   document.getElementById("subT").innerHTML = "USD " + subtotal;
+   document.getElementById("total").innerHTML = "USD " + subtotal;
+}
+function envio2() {
+    let subtotal = [];
+    subtotal = mostrarSubtotal * 1.07;
+    document.getElementById("subT").innerHTML = "USD " + subtotal;
+    document.getElementById("total").innerHTML = "USD " + subtotal;
+}
+function envio3() {
+    let subtotal = [];
+    subtotal = mostrarSubtotal * 1.05;
+    document.getElementById("subT").innerHTML = "USD " + subtotal;
+    document.getElementById("total").innerHTML = "USD " + subtotal;
+}
+
 
 //funcion que muestra el valor a pagar en el total
 function totalFinal() {
@@ -55,15 +72,38 @@ function totalFinal() {
     document.getElementById("total").innerHTML = "USD " + total;
 }
 
-//funcion que activa un evento change para que el valor del sub total se modifique cuando se modifica la cantidad de articulos
+//funcion que activa un evento change para cambiar el valor del sub total  cuando se modifica la cantidad de articulos
 function addEventCount() {
     let subtotalArray = document.getElementsByClassName("article");
     for (let i = 0; i < subtotalArray.length; i++) {
         subtotalArray[i].addEventListener("change", function () {
             document.getElementById("productSubtotal-" + i).innerHTML = arrayArticles[i].currency + " " + subtotalArray[i].value * arrayArticles[i].unitCost;
             updateAllSubTotal();
-            totalFinal();
+            totalFinal();            
         });
+    }      
+}
+
+// validacion: toma los valores de los campos deseados, y avisa si faltan valores para llenar
+function validar() {
+    let calle = document.getElementById("calle").value;
+    let numero = document.getElementById("numero").value;
+    let esquina = document.getElementById("esquina").value;
+    if (calle === "" && numero === "" && esquina ==="") {
+        alert("faltan llenar campos");
+    } else {
+        alert("todo bien");
+    }
+}
+
+function validar2() {
+    let moneda = document.getElementById("moneda").value;
+    let tarjeta = document.getElementById("tarjeta").value;
+    let numero = document.getElementById("numero").value;
+    if (moneda === "" && tarjeta === "" && numero === "") {
+        alert("faltan llenar campos");
+    } else {
+        alert("todo bien");
     }
 }
 
@@ -73,6 +113,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (resultObj.status === 'ok') {
             arrayArticles = resultObj.data.articles;
             showCartProductsAndTotalCost(arrayArticles);
+            //validar();
         }
     });
 })
